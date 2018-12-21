3118e883aa2c66435c9c7ac03c75920b36880b56
tejas-niturkar <tejas.niturkar@gmail.com>
2018-12-21 11:55:01 +0530

    added test case for form page

--- src/main/java/com/crm/qa/pages/FormsPage.java
+++ src/main/java/com/crm/qa/pages/FormsPage.java
@@ -1,4 +1,29 @@
 package com.crm.qa.pages;
 
-public class FormsPage {
+import com.crm.qa.base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FormsPage extends TestBase {
+
+    @FindBy (xpath = "//td[contains(text(), 'Feedback Forms')]")
+    WebElement feedbackFormsLabel ;
+
+
+
+    FormsPage(){
+        PageFactory.initElements(driver,this);
+    }
+
+
+    public boolean validateFormsPageLabel() throws InterruptedException {
+        Thread.sleep(1000);
+        return feedbackFormsLabel.isDisplayed();
+    }
+
+
+
+
+
 }
--- src/test/java/com/crm/qa/testcases/FormsPageTest.java
+++ src/test/java/com/crm/qa/testcases/FormsPageTest.java
@@ -1,4 +1,41 @@
 package com.crm.qa.testcases;
+import com.crm.qa.base.TestBase;
+import com.crm.qa.pages.FormsPage;
+import com.crm.qa.pages.HomePage;
+import com.crm.qa.pages.LoginPage;
+import com.crm.qa.utils.TestUtils;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-public class FormsPageTest {
+public class FormsPageTest extends TestBase {
+
+    LoginPage loginPage;
+    HomePage homePage ;
+    TestUtils testUtils;
+    FormsPage formsPage;
+
+    String sheetName = "forms" ;
+
+    FormsPageTest() { super(); }
+
+    @BeforeMethod
+    public void setUp(){
+        initialization();
+        testUtils = new TestUtils();
+        loginPage = new LoginPage();
+        homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+        testUtils.switchToFrame();
+        formsPage = homePage.clickOnFormsLink();
+    }
+
+
+    @Test (priority = 1)
+    public void validateFormsPageLabel() throws InterruptedException {
+        Assert.assertTrue(formsPage.validateFormsPageLabel(), "Label not found");
+    }
+
+
+
+
 }
